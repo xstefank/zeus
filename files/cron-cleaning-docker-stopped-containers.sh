299c361daba293b0a5e7f74a20665395b4d1d8d0
Romain Pelisse <belaran@gmail.com>
2018-03-14 15:55:03 +0100

    SET-73 - Enhance cleaning scripts

--- files/cron-cleaning-docker-stopped-containers.sh
+++ files/cron-cleaning-docker-stopped-containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 date +%Y/%m/%d-%H:%M
-for container in $(docker ps --filter "status=exited"  | sed -e 's/^\([^ ]*\).*$/\1/');
+for container in $(docker ps --filter "status=exited"  | sed -e 's/^\([^ ]*\).*$/\1/' | sed -e '/CONTAINER/d' );
 do
   echo -n "Removing container $(docker inspect --format "{{ .Name}}" ${container}): "
   docker rm "${container}"
