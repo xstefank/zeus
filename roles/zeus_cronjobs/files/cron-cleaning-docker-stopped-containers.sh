4a69f2c28cdf35657adf5172d7233398e59871ae
Romain Pelisse <belaran@redhat.com>
2021-02-19 06:09:25 -0500

    Remove deprecated role zeus_cronjobs

--- roles/zeus_cronjobs/files/cron-cleaning-docker-stopped-containers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-date '+%Y/%m/%d-%H:%M' 
-for container in $(docker ps --filter "status=exited"  | sed -e 's/^\([^ ]*\).*$/\1/' | sed -e '/CONTAINER/d' );
-do
-  echo -n "Removing container $(docker inspect --format "{{ .Name}}" ${container}): "
-  docker rm "${container}"
-  echo 'Done.'
-done
